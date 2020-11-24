e34ff98835b6a8110f8b0bb36e1b829a3d542a87
jlopez <jonathanglopez@gmail.com>
2020-11-24 14:22:44 -0500

    Added support for default lsp installations

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -13,35 +13,26 @@ lspconfig.gopls.setup{
   root_dir = lspconfig.util.root_pattern('.git');
 }
  
-lspconfig.texlab.setup{
-  on_attach=full_attach;
-  name = 'texlab_fancy';
-  log_level = vim.lsp.protocol.MessageType.Log;
-  message_level = vim.lsp.protocol.MessageType.Log;
-  settings = {
-    latex = {
-      build = {
-        onSave = true;
-      }
-    }
-  }
-}
-
 -- lspconfig.jdtls.setup{
   -- on_attach = full_attach;
   -- root_dir = lspconfig.util.root_pattern(".git", "build.gradle", "build.gradle.kts", "pom.xml");
 -- }
 
-lspconfig.dockerls.setup{on_attach=full_attach}
 lspconfig.vimls.setup{on_attach=full_attach}
 lspconfig.pyls_ms.setup{on_attach=full_attach}
-lspconfig.clangd.setup{on_attach=full_attach}
 lspconfig.clangd.setup{on_attach=full_attach}
 lspconfig.julials.setup{on_attach=full_attach}
 lspconfig.metals.setup{on_attach=full_attach}
 lspconfig.leanls.setup{on_attach=full_attach}
 lspconfig.yamlls.setup{on_attach=full_attach}
 lspconfig.sqlls.setup{on_attach=full_attach}
+lspconfig.bashls.setup{on_attach=full_attach}
+lspconfig.html.setup{on_attach=full_attach}
+lspconfig.jsonls.setup{on_attach=full_attach}
+lspconfig.sumneko_lua.setup{on_attach=full_attach}
+lspconfig.tsserver.setup{on_attach=full_attach}
+
+
 
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
