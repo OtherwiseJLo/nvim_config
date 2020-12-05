e2bc0c4cb3ac7a29e2280418822f94be987c3d2e
OtherwiseJLo <jonathanglopez@gmail.com>
2020-12-05 12:40:06 -0500

    Updated paqs file name

---
+++ ftdetect/vimwiki.vim
@@ -0,0 +1,3 @@
+set spell
+hi clear SpellBad
+hi SpellBad cterm=underline,bold ctermfg=red ctermbg=black
---
+++ lua/paqs.lua
@@ -0,0 +1,82 @@
+vim.cmd 'packadd paq-nvim'
+local paq = require'paq-nvim'.paq
+
+paq 'neovim/nvim-lspconfig'
+paq 'neomake/neomake'
+
+paq 'RishabhRD/popfix'
+paq 'RishabhRD/nvim-lsputils'
+
+paq 'unblevable/quick-scope'
+
+paq 'nvim-lua/completion-nvim'
+paq 'nvim-lua/popup.nvim'
+paq 'nvim-lua/plenary.nvim'
+paq 'nvim-lua/telescope.nvim'
+paq 'nvim-lua/lsp-status.nvim'
+
+paq 'nvim-treesitter/nvim-treesitter'
+paq 'nvim-treesitter/nvim-treesitter-textobjects'
+paq 'nvim-treesitter/completion-treesitter'
+paq 'nvim-treesitter/nvim-treesitter-refactor'
+paq 'nvim-treesitter/playground'
+
+
+paq 'itchyny/lightline.vim'
+paq 'tpope/vim-vinegar'
+paq 'tpope/vim-fugitive'
+paq 'voldikss/vim-floaterm'
+paq 'vimwiki/vimwiki'
+paq 'sbdchd/neoformat'
+-- paq 'romgrk/barbar.nvim'
+
+
+-- Databases
+paq 'tpope/vim-dadbod'
+paq 'kristijanhusak/vim-dadbod-ui'
+paq 'kristijanhusak/vim-dadbod-completion'
+
+
+paq 'wellle/visual-split.vim'
+paq 'hrsh7th/vim-vsnip'
+paq 'hrsh7th/vim-vsnip-integ'
+
+-- paq 'junegunn/fzf', { 'do': { -> fzf#install() } }
+-- paq 'junegunn/fzf.vim'
+
+paq 'glepnir/dashboard-nvim'
+
+-- The bang version will try to download the prebuilt binary if cargo does not exist.
+-- paq 'liuchengxu/vim-clap', { 'do': ':Clap install-binary!' }
+paq 'liuchengxu/vim-clap'
+
+
+-- Colorschemes
+paq 'glepnir/zephyr-nvim'
+paq 'arcticicestudio/nord-vim'
+paq 'christianchiarulli/nvcode-color-schemes.vim'
+
+paq 'ThePrimeagen/vim-be-good'
+
+-- Markdown preview
+paq 'npxbr/glow.nvim'
+
+-- Adding auto complete supports
+paq 'tpope/vim-endwise'
+paq 'jiangmiao/auto-pairs'
+
+-- Commentary
+paq 'preservim/nerdcommenter'
+paq 'mfussenegger/nvim-jdtls'
+
+-- Git integration
+paq 'airblade/vim-gitgutter'
+
+paq 'udalov/kotlin-vim'
+
+paq 'skywind3000/asynctasks.vim'
+paq 'skywind3000/asyncrun.vim'
+
+paq 'bfredl/nvim-ipy'
+paq 'sainnhe/sonokai'
+
